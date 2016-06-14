d6e976616cd61d25d5a8765d60ad23c03412160e
Jonathan Salazar Garcia <Jonathan Salazar Garcia>
2016-06-14 15:08:56 -0600

    making a basic TicTacToe on angular

--- Web Test/js/Controller/controller.js
+++ Web Test/js/Controller/controller.js
@@ -3,10 +3,48 @@ app.controller("MyController",function($scope)
 $scope.title="We are playing TicTacToe, choose your symbol";
 $scope.TicTacToe=
 [
-' ',' ',' ',' ',' ',' ',' ',' ',' '
+'Empty','Empty','Empty','Empty','Empty','Empty','Empty','Empty','Empty'
 ];
-$scope.uncheck = function (event) {
-    if ($scope.checked == event.target.value)
-        $scope.checked = false
+$scope.checked=true;
+$scope.UserSymbol='';
+$scope.ComputerSymbol='';
+$scope.ChangeSymbol= function(letter)
+{
+	if(letter == 'X'){
+		$scope.UserSymbol=letter;
+		$scope.ComputerSymbol='O';
+	}
+	else{
+		$scope.UserSymbol=letter
+		$scope.ComputerSymbol='X';
+	}
+
+};
+$scope.UserPlaying= function(index)
+{
+	if($scope.TicTacToe[index]=='Empty')
+	{
+		$scope.TicTacToe[index]=$UserSymbol;
+		ComputerPlaying();
+	}
+	else
+	{
+		window.alert("Click On over an empty spot");
+	}
+
+	ComputerPlaying();
+};
+
+$scope.ComputerPlaying= function()
+{
+	var index = Math.floor(Math.random() * 9);
+	if($scope.TicTacToe[index]=='Empty')
+	{
+		$scope.TicTacToe[index]=$ComputerSymbol;
+	}
+	else
+	{
+		ComputerPlaying();
+	}
 };
 });
