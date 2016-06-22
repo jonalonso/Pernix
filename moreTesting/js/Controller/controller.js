b4d04cc12d7751a911f9e377cf23a5b10e6d471d
jonalonso <salazargarciajonathan@gmail.com>
2016-06-22 14:09:34 -0600

    To Do List Project main Page Done

--- moreTesting/js/Controller/controller.js
+++ moreTesting/js/Controller/controller.js
@@ -1,5 +1,27 @@
 app.controller("MyController",function($scope)
 {
+$scope.user="Jonathan"
+$scope.newAction="";
+$scope.todo=[
+];
+$scope.addElementToDo=function()
+{
+	if($scope.newAction!=""){
+		var newElement={action: $scope.newAction,done:false};
+		$scope.todo.push(newElement);
+		$scope.newAction="";
+	}
+	else
+	{
+		window.alert("Blank tasks are not allowed");
+	}
+};
 
+$scope.deleteTask=function($index)
+{
+
+	$scope.todo.splice($index,1);
+
+}
 
 });
---
+++ moreTesting/js/filter/filter.js
@@ -0,0 +1,8 @@
+app.filter('true_false', function() {
+    return function(text, length, end) {
+        if (text) {
+            return 'Yes';
+        }
+        return 'No';
+    }
+});
